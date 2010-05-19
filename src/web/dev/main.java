a27a817f2b3f8d0d8a6e79b904dd3676836d8d1e
ACR <ACR@bxl-laptop158.SXPBELUX.NET>
2010-05-20 00:44:08 +0200

    changes

--- src/web/dev/main.java
+++ src/web/dev/main.java
@@ -2,12 +2,16 @@ package web.dev;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello Android");
+        setContentView(tv);
+        /**@todo test*/
     }
 }
